7247d3862f0e501e58f9144ee5cd45f64cb32f53
Emmanuel <demey.emmanuel@gmail.com>
2026-02-05 14:56:34 +0000

    feat: add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lille-tech-communities.netlify.app",
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("/draft/"),
+    }),
+    robotsTxt(),
+  ],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
